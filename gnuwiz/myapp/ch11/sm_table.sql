ad5e4a31ae2537acae6c0e4dad8985f759c6d50a
Gnuwiz <87157885+gnuwiz@users.noreply.github.com>
2021-08-17 10:57:18 +0900

    챕터 11 SQL파일

---
+++ gnuwiz/myapp/ch11/sm_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE sm_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    affiliation INT NOT NULL
+);
+
+INSERT INTO sm_table VALUES (3, 'SM', 3);
+INSERT INTO sm_table VALUES (6, '솔로', 3);
+INSERT INTO sm_table VALUES (8, '그룹', 3);
+INSERT INTO sm_table VALUES (9, '보아', 6);
+INSERT INTO sm_table VALUES (2, '소녀시대', 8);
+INSERT INTO sm_table VALUES (4, '슈퍼주니어', 8);
+INSERT INTO sm_table VALUES (11, 'EXO', 8);
