cf11d8df7f199152706f1482c41210668398e9ed
Gnuwiz <87157885+gnuwiz@users.noreply.github.com>
2021-08-17 10:58:43 +0900

    챕터 16 소스코드+SQL파일

---
+++ gnuwiz/myapp/ch16/dbconn.php
@@ -0,0 +1,13 @@
+<?php
+$mysql_host = "localhost";
+$mysql_user = "root";
+$mysql_password = "1234";
+$mysql_db = "project";
+// MySQL 데이터베이스 연결
+$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
+
+if (!$conn) { // 연결 오류 발생 시 스크립트 종료
+    die("연결 실패: " . mysqli_connect_error());
+}
+
+ini_set('display_errors', 'Off');
